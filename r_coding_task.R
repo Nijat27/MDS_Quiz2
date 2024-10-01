0eefbd8ce2a485c3887ab6b1863461f329aa0100
Nijiati_Abulizi <114438964+Nijat27@users.noreply.github.com>
2024-10-01 15:32:52 -0700

    Add files via upload

---
+++ r_coding_task.R
@@ -0,0 +1,54 @@
+
+# R Coding Task
+
+# Instructions:
+
+# 1. Use the `rquizdata.csv` file and read the data into a data frame. (1 mark)
+# Hint: You can use the `read.table()` or `read.csv()` function.
+# Your code here for Task 1
+
+
+# 2. Display the first 5 rows and the last 3 rows of the data frame. (1 mark)
+# Your code here for Task 2
+
+
+# 3. Add a new column to the data frame called `fullname` that concatenates `firstname` and `lastname` 
+# with a space in between. (1 mark)
+# Hint: Use the `paste()` function to concatenate strings.
+# Your code here for Task 3
+
+
+# 4. Add a new column to the data frame called `management` that assigns the value "Y" if the person 
+# has a salary greater than 50,000 and is older than 45, otherwise assign "N". Print the first 5 rows 
+# again to show the updated data frame. (1 mark)
+# Hint: Use the `ifelse()` function to assign values based on a condition.
+# Your code here for Task 4
+
+
+# 5. Calculate and print the average age and the maximum salary. (1 mark)
+# Your code here for Task 5
+
+
+# 6. Calculate and print the average salary for females and males. (1 mark)
+# Hint: You can use one of the functions: `apply(), tapply(), lapply(), sapply(), and mapply()` .
+# Your code here for Task 6
+
+
+# 7. Display a bar chart showing the number of females and males. (1 mark)
+# Note: Use `ggplot2` and `geom_bar()` for the chart, but any charting library is acceptable.
+# Your code here for Task 7
+
+
+# 8. Print the people who earn less than $35,000 or are younger than 21. (1 mark)
+# Your code here for Task 8
+
+
+# 9. Create boxplots to visualize the salary distribution for females and males. (1 mark)
+# Note: Use `ggplot2` and `geom_boxplot()` for the chart, but any charting library is acceptable.
+# Your code here for Task 9
+
+
+# 10. Create a function called `buildVector` that takes a number `n` as a parameter and builds and 
+# returns a vector with the numbers from 1 to `n`. Test your function for values `n = 3` and `n = 7` 
+# and print out the returned vector. (1 mark)
+# Your code here for Task 10
